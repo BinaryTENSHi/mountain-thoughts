e8c53a7457f9866277bdaffbab5df2b036b18617
BinaryTENSHi <binarytenshi@gmail.com>
2015-01-18 18:17:25 +0100

    Threaded reading, some refactoring

--- mountain-thoughts/NativeMethods.cs
+++ mountain-thoughts/NativeMethods.cs
@@ -1,10 +1,92 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
 
 namespace mountain_thoughts
 {
     public static class NativeMethods
     {
+        private static bool _called;
+        private static string _lastString;
+
+        private static readonly string[] Exclusions =
+        {
+            string.Empty,
+            "SAVING... "
+        };
+
+        private static bool _reading;
+        private static Thread _readingThread;
+
+        public static Process GetMountainProcess()
+        {
+            return Process.GetProcessesByName("Mountain").FirstOrDefault();
+        }
+
+        public static IntPtr GetStringAddress(Process process)
+        {
+            IntPtr handle = process.Handle;
+            IntPtr baseAddress = process.MainModule.BaseAddress;
+
+            byte[] ptrBuffer = new byte[4];
+            IntPtr byteread = new IntPtr(-1);
+            IntPtr address = IntPtr.Add(baseAddress, MemoryValues.Base);
+
+            foreach (int offset in MemoryValues.Offsets)
+            {
+                Console.WriteLine("Reading Address 0x" + address.ToString("X"));
+                ReadProcessMemory(handle, address, ptrBuffer, ptrBuffer.Length, out byteread);
+                Debug.Assert(byteread.ToInt32() == 4);
+                address = (IntPtr) (BitConverter.ToInt32(ptrBuffer, 0) + offset);
+            }
+
+            return address;
+        }
+
+        public static void StartReadingString(IntPtr handle, IntPtr stringAddress, Action<string> act)
+        {
+            _reading = true;
+            _readingThread = new Thread(() => ReadingLoop(handle, stringAddress, act));
+            _readingThread.Start();
+        }
+
+        public static void StopReadingString()
+        {
+            _reading = false;
+            _readingThread.Join();
+        }
+
+        public static void ReadingLoop(IntPtr handle, IntPtr stringAddress, Action<string> act)
+        {
+            IntPtr byteread = new IntPtr(-1);
+            byte[] stringBuffer = new byte[128];
+
+            do
+            {
+                ReadProcessMemory(handle, stringAddress, stringBuffer, stringBuffer.Length, out byteread);
+                if (stringBuffer[0] != 0x00 && ParseString(stringBuffer) && !_called)
+                {
+                    act(_lastString);
+                    _called = true;
+                }
+                Thread.Sleep(500);
+            } while (_reading);
+        }
+
+        private static bool ParseString(byte[] stringBuffer)
+        {
+            string currString = Encoding.ASCII.GetString(stringBuffer).Split('\0')[0];
+            currString = currString.Replace("\n", string.Empty).Trim();
+            if (_lastString == currString && !Exclusions.Contains(currString))
+                return true;
+            _called = false;
+            _lastString = currString;
+            return false;
+        }
+
         [DllImport("kernel32.dll", SetLastError = true)]
         public static extern bool ReadProcessMemory(
             IntPtr hProcess,
--- mountain-thoughts/Program.cs
+++ mountain-thoughts/Program.cs
@@ -1,72 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading;
 
 namespace mountain_thoughts
 {
     public class Program
     {
-        private static bool _printed;
-        private static string _prevString;
-
-        public static int Main(string[] args)
+        public static void Main(string[] args)
         {
-            Process process = Process.GetProcessesByName("Mountain").FirstOrDefault();
-            ;
-            if (process == null)
+            Process mountainProcess = NativeMethods.GetMountainProcess();
+            if (mountainProcess == null)
             {
-                Console.WriteLine("Could not find Mountain process.");
-                return 1;
+                Console.WriteLine("Could not get process. Is Mountain running?");
+                Console.ReadLine();
+                Environment.Exit(1);
             }
 
-            IntPtr handle = process.Handle;
-            IntPtr baseAddress = process.MainModule.BaseAddress;
+            IntPtr handle = mountainProcess.Handle;
+            IntPtr address = NativeMethods.GetStringAddress(mountainProcess);
 
-            Console.WriteLine("Handle: 0x" + handle.ToString("X"));
-            Console.WriteLine("Base: 0x" + baseAddress.ToString("X"));
-
-            byte[] ptrBuffer = new byte[4];
-            IntPtr byteread = new IntPtr(-1);
-            IntPtr address = IntPtr.Add(baseAddress, MemoryValues.Base);
-
-            List<int> offsets = new List<int> {MemoryValues.Base};
-            offsets.AddRange(MemoryValues.Offsets);
-
-            foreach (int offset in MemoryValues.Offsets)
-            {
-                Console.WriteLine("Reading Address 0x" + address.ToString("X"));
-                NativeMethods.ReadProcessMemory(handle, address, ptrBuffer, ptrBuffer.Length, out byteread);
-                Debug.Assert(byteread.ToInt32() == 4);
-                address = (IntPtr) (BitConverter.ToInt32(ptrBuffer, 0) + offset);
-            }
-
-            Console.WriteLine("Final Address 0x" + address.ToString("X"));
-
-            byte[] stringBuffer = new byte[64];
-
-            do
-            {
-                NativeMethods.ReadProcessMemory(handle, address, stringBuffer, stringBuffer.Length, out byteread);
-                if (stringBuffer[0] != 0x00 && ParseString(stringBuffer) && !_printed)
-                {
-                    Console.WriteLine(_prevString);
-                    _printed = true;
-                }
-                Thread.Sleep(100);
-            } while (true);
+            NativeMethods.StartReadingString(handle, address, Callback);
+            Console.ReadLine();
+            NativeMethods.StopReadingString();
         }
 
-        private static bool ParseString(byte[] stringBuffer)
+        private static void Callback(string thought)
         {
-            string currString = Encoding.ASCII.GetString(stringBuffer).Split('\0')[0];
-            if (_prevString == currString && currString != "SAVING... ")
-                return true;
-            _printed = false;
-            _prevString = currString;
-            return false;
+            Console.WriteLine(thought);
         }
     }
 }
